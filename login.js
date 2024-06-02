8f97621257de0cea350e75fec0b052a253a9ae12
Biman Pakhira <56791618+Biman-pakhira@users.noreply.github.com>
2024-06-02 12:22:08 +0000

    Commit

---
+++ login.js
@@ -0,0 +1,28 @@
+let form=document.querySelector("form");
+   
+
+   let signupls=JSON.parse(localStorage.getItem("signup"))||[]
+   form.addEventListener("submit",function(e){
+        e.preventDefault()
+        if(signupls.length==0){
+            alert("No user till now")
+            return;
+        }
+        let obj={
+            mblno:form.mbl.value
+        }
+      signupls.forEach(function(ele){
+        if(ele.mblno===obj.mblno){
+            localStorage.setItem("login",JSON.stringify(ele));
+            alert("Login Successfull")
+            window.location.href="home.html"
+            return;
+        }
+        else{
+            alert("User does not Exist")
+            return;
+        }
+       
+      })
+        
+    })
